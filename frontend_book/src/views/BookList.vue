f0d111e7860b1bc17b5661a38606190876ae143b
Chungho Lee <chlee125@gmail.com>
2018-11-22 22:13:26 +0900

    BookList.vue 스크립트 추가

---
+++ frontend_book/src/views/BookList.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex md12>
+
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data:function() {
+    return {
+      items = [];
+    }
+  },
+  mounted() {
+    console.log("mount");
+    axios.get("http://localhost:3000/books").then(result => {
+      console.log(result);
+    });
+  }
+}
+
+</script>
